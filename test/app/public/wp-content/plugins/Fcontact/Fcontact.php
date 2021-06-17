52e767d026b36699c116036cf6dd03292b51d11d
fadoua <fadwakharoube00@gmail.com>
2021-06-17 09:54:29 +0100

    plugin

---
+++ test/app/public/wp-content/plugins/Fcontact/Fcontact.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:       Fcontact
+ * Plugin URI:        https://Fcontact.com/plugin
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Author:            Fadoua Kharroub
+ * Author URI:        https://Fadoua.kharroub.com/
+ * License:           GPL v2 or later
+ * Text Domain:       Fcontact
